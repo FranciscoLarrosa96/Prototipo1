68651a9525a832f6aa52ff21a506a920ca80cd0f
Francisco Larrosa <f.larrosa@sieteideas.com.ar>
2024-06-10 11:56:53 -0300

    Route Home added

--- FirstApp/src/app/app.routes.ts
+++ FirstApp/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: 'home',
+        loadComponent: () => import('./components/home/home.component').then(component => component.HomeComponent)
+    },
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    }
+];
