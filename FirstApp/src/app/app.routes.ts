b04cf900c2ee31d09ce97b243fb239e4f536c6bc
Francisco Larrosa <f.larrosa@sieteideas.com.ar>
2024-06-10 16:04:56 -0300

    Routes updated

--- FirstApp/src/app/app.routes.ts
+++ FirstApp/src/app/app.routes.ts
@@ -10,5 +10,9 @@ export const routes: Routes = [
         path: '',
         redirectTo: '/home',
         pathMatch: 'full'
+    },
+    {
+        path : '**',
+        redirectTo : '/home'
     }
 ];
