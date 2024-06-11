b14360aa1c0d64fbd4d858abcb82d76e25df2a18
Francisco Larrosa <franciscolarrosa22@gmail.com>
2024-06-11 18:09:08 -0300

    Visual changes

--- FirstApp/src/app/shared/title/title.component.ts
+++ FirstApp/src/app/shared/title/title.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { MaterialModule } from '../material.module';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-title',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule, ReactiveFormsModule],
   templateUrl: './title.component.html',
   styleUrl: './title.component.scss'
 })
-export class TitleComponent {
+export class TitleComponent implements OnInit {
+  showInputSearch: boolean = false;
+  ngOnInit(): void {
+
+  }
 
 }
