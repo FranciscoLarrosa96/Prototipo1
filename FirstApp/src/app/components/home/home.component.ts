623e6c3d57b1e23834e3c014fff96614f4f23256
Francisco Larrosa <franciscolarrosa22@gmail.com>
2024-06-11 23:08:37 -0300

    Many visual changes

--- FirstApp/src/app/components/home/home.component.ts
+++ FirstApp/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from '../../interfaces/product.interface';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +9,46 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  
+  items: Product[] = [
+    {
+      img: 'assets/img/products/a620.jpg',
+      title: 'Mother A620 AM4',
+      description: ''
+    },
+    {
+      img: 'assets/img/products/a620m-k.jpg',
+      title: 'Mother A620',
+      description: ''
+    },
+    {
+      img: 'assets/img/products/b650-a.jpg',
+      title: 'Mother A620',
+      description: ''
+    },
+    {
+      img: 'assets/img/products/b650m-a.jpg',
+      title: 'Mother A620',
+      description: ''
+    },
+    {
+      img: 'assets/img/products/b650m-h.webp',
+      title: 'Mother A620',
+      description: ''
+    },
+    {
+      img: 'assets/img/products/x670.jpg',
+      title: 'Mother A620',
+      description: ''
+    },
+    {
+      img: 'assets/img/products/x670e.png',
+      title: 'Mother A620',
+      description: ''
+    }
+  ];
 
-  constructor(){
+  constructor() {
 
   }
-    
+
 }
