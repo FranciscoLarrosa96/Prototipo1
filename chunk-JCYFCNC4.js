0b0c1d3bec5af7f04af2346784b4179b7a5791dd
Francisco Larrosa <f.larrosa@sieteideas.com.ar>
2024-11-21 09:58:45 -0300

    Auto-generated commit

---
+++ chunk-JCYFCNC4.js
@@ -0,0 +1,1 @@
+import{Ab as a,Bb as r,Wb as i,bc as s,ia as o}from"./chunk-GSD3XDFW.js";var d=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=o({type:e,selectors:[["app-cart"]],standalone:!0,features:[s],decls:2,vars:0,template:function(n,p){n&1&&(a(0,"p"),i(1,"cart works!"),r())}});let t=e;return t})();export{d as CartComponent};
