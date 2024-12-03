41bbf34626ad59ec931beb0375b9faafdcaae763
Francisco Larrosa <fran.7id@gmail.com>
2024-12-03 14:31:34 -0300

    Feat : Environment PROD added

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    API_AUTH: 'https://ecommerce2024-backend-production.up.railway.app/api/auth',
+};
+// Para generar el build usar este comando "ng build --configuration=prod"
