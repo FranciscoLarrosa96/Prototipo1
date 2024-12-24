c9b8280fb2e74d14efaf95cbc30638cdc48dabd8
Francisco Larrosa <fran.7id@gmail.com>
2024-12-23 22:56:26 -0300

    Fix  : Se quito user service

--- src/app/pages/profile/profile.ts
+++ src/app/pages/profile/profile.ts
@@ -1,10 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, effect, inject, OnInit } from '@angular/core';
 import { MaterialModule } from '../../shared/material.module';
 import { Router } from '@angular/router';
 import { AuthService } from '../../shared/services/auth.service';
-import { UserService } from '../../shared/services/user.service';
-import { User } from '../../interfaces/user.interface';
 
 declare const google: any;
 
@@ -16,9 +14,13 @@ declare const google: any;
 })
 export class ProfileComponent implements OnInit {
 
+  effectUser = effect(() => {
+    this._authSvc.userLoggedComputed();
+    console.log('userLoggedComputed', this._authSvc.userLoggedComputed());
+    
+  });
   private _router = inject(Router);
   private _authSvc = inject(AuthService);
-  private _userSvc = inject(UserService);
 
   constructor() { }
 
@@ -26,11 +28,6 @@ export class ProfileComponent implements OnInit {
   ngOnInit(): void {
     google.accounts.id.initialize({
       client_id: '684557034764-dot47pevhl29b9q3koj83vnkkim2v26l.apps.googleusercontent.com',
-    });
-    this._userSvc.getUserInfo().subscribe({
-      next : (user:User) => {
-        console.log('User', user);
-      }
     });
   }
 
