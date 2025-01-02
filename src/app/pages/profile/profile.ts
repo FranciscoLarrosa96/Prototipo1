138dce634ef7bcb5cf2bf6895b8edae345f5d3cf
Francisco Larrosa <fran.7id@gmail.com>
2025-01-02 17:47:38 -0300

    Test user logged

--- src/app/pages/profile/profile.ts
+++ src/app/pages/profile/profile.ts
@@ -1,27 +1,29 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, JsonPipe } from '@angular/common';
 import { AfterViewInit, Component, effect, inject, OnInit } from '@angular/core';
 import { MaterialModule } from '../../shared/material.module';
 import { Router } from '@angular/router';
 import { AuthService } from '../../shared/services/auth.service';
+import { User } from '../../interfaces/user.interface';
 
 declare const google: any;
 
 @Component({
   selector: 'app-profile',
-  imports: [CommonModule, MaterialModule],
+  imports: [CommonModule, MaterialModule, JsonPipe],
   templateUrl: './profile.html',
   styleUrl: './profile.scss',
 })
 export class ProfileComponent implements OnInit, AfterViewInit {
-
+  userLogged!: User;
   private _router = inject(Router);
   private _authSvc = inject(AuthService);
 
   constructor() { }
-  
+  //TODO:"Arreglar probar llamar service en un resolve router"
   ngAfterViewInit(): void {
     setTimeout(() => {
       console.log('userLoggedComputed', this._authSvc.userLoggedComputed());
+      this.userLogged = this._authSvc.userLoggedComputed();
     }, 100);
   }
 
