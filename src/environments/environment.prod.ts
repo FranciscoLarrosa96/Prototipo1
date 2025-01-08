2e318a54eeb3a476aebc9a0198781942df69b98c
FranciscoLarrosa <Franciscolarrosa22@gmail.com>
2025-01-08 19:42:10 -0300

    Feat: Se cambio endpoints

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    API_AUTH: 'https://ecommerce2024-backend-production.up.railway.app/api',
-    API_USER: 'https://ecommerce2024-backend-production.up.railway.app/api/user',
+    API_AUTH: 'https://ecommerce2024-backend.onrender.com/api',
+    API_USER: 'https://ecommerce2024-backend.onrender.com/api/user',
 };
 // Para generar el build usar este comando "ng build --configuration=prod" esto genera el dist para usar en la carpeta static del backendque se despliegue en produccion
 // Para generar el build para github pages usar este comando "ng build --configuration=github-pages" y luego npx angular-cli-ghpages --dir=dist/ecommerce/browser para subir a github pages
